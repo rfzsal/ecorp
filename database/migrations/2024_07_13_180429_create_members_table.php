9f0296876bf63e25d928a642f093f23e39476349
Faizal <58117968+rfzsal@users.noreply.github.com>
2024-07-14 02:51:04 +0700

    add member group and member model

---
+++ database/migrations/2024_07_13_180429_create_members_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone')->unique();
+            $table->foreignId('user_id')->unique()->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('member_group_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
